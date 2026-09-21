c8d7aa16a20d1da4cad11d198533bbd546ba4260
Vulluri Yamini <325103310240@gvpce.ac.in>
2026-09-21 18:19:46 +0530

    Create difference diagonal.py
    Solved the HackerRank Diagonal Difference problem by calculating the sums of the primary and secondary diagonals of a square matrix and finding their absolute difference.

---
+++ difference diagonal.py
@@ -0,0 +1,49 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    x=0
+    y=0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if i==j:
+                
+            
+                x=x+arr[i][j]
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if i==j:
+            
+                y=y+arr[i][len(arr)-j-1]
+    return abs(x-y)
+            
+    
+
+            
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
