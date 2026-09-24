4a13628277870e2f79fa9460fb4c8a935e41ec5c
Vulluri Yamini <325103310240@gvpce.ac.in>
2026-09-24 13:56:35 +0530

    Create grading students.py
    solve the HackerRank grading students problem by using problems using loops and conditional statements

---
+++ grading students.py
@@ -0,0 +1,81 @@
+HackerLand University has the following grading policy:
+
+Every student receives a  in the inclusive range from  to .
+Any  less than  is a failing grade.
+Sam is a professor at the university and likes to round each student's  according to these rules:
+
+If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+Examples
+
+ round to  (85 - 84 is less than 3)
+ do not round (result is less than 38)
+ do not round (60 - 57 is 3 or higher)
+Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
+
+Function Description
+
+Complete the function  with the following parameter(s):
+
+: the grades before rounding
+Returns
+
+: the grades after rounding
+
+'''input'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    lst1=[]
+
+    lst2=[]
+    for grade in grades:
+        if grade<38:
+            lst1.append(grade)
+        else:
+            lst2.append(grade)
+            
+            for i in range(len(lst2)):    
+                for j in range(lst2[i]+1,lst2[i]+4):
+                    if j%5==0:      
+                        if j-lst2[i]<3:
+                            lst2[i]=j
+                         
+                        
+                        break
+        
+    return lst2+lst1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
